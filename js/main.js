50ddf531304e7207b11033fd7b720d44b841580d
FinzyPHINZY <finzyphinzy@gmail.com>
2023-06-23 10:52:21 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//Create a functionality that displays and alert the current age of a user after the user inputs the year of birth
+
+//first step: utilize an event listener that will be called after the button have been clicked and runs a fucntion
+
+//second step: create a variable that represents the input of the current year
+
+//third step: use the javascript method to get the current date and current year. 
+
+//fourth step:create another variable that will subtract the current year from the birth year
+
+//last step: use js to update the innerhtml of the output to reflect the result of the previous step
+
+document.querySelector('button').addEventListener('click', getAge);
+
+
+function getAge() {
+    const birthyear = Number(document.getElementById('year').value);
+
+    const now =  new Date()
+
+    let today = now.getFullYear();
+
+    const age = today - birthyear;
+
+
+    document.getElementById('output').innerText = age;
+
+}
+
+
+
+
